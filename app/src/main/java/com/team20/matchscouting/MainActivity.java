bef9afc860d0d30c641e2f1fdc191f33988597ca
Ben Hogan <bhemail14@yahoo.com>
2018-01-07 19:09:18 -0500

    LoLZ
    I Did It For The LOLZ

--- app/src/main/java/com/team20/matchscouting/MainActivity.java
+++ app/src/main/java/com/team20/matchscouting/MainActivity.java
@@ -1,7 +1,20 @@
 package com.team20.matchscouting;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 /**
  * Created by JT on 1/7/18.
  */
-public class MainActivity {
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void nextButtonOnClick(View view){
+
+    }
 }
