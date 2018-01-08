7c3d57542fd653ab57e78606c0ff00b0eb6463ba
Ben Hogan <bhemail14@yahoo.com>
2018-01-07 20:29:29 -0500

    Part of Auto
    Auto in process (Not Completed)

--- app/src/main/java/com/team20/matchscouting/MainActivity.java
+++ app/src/main/java/com/team20/matchscouting/MainActivity.java
@@ -1,5 +1,6 @@
 package com.team20.matchscouting;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity{
     }
 
     public void nextButtonOnClick(View view){
-
+        Intent intent = new Intent(MainActivity.this,ScoutScreen.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/team20/matchscouting/ScoutScreen.java
@@ -0,0 +1,16 @@
+package com.team20.matchscouting;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Ben on 1/7/2018.
+ */
+
+public class ScoutScreen extends Activity{
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.scout_screen);
+    }
+}
