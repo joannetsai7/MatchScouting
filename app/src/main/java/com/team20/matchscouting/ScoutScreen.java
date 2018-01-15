fbd887b9b4bd8493d319e76fff96db5ab5aa24f3
Ben Hogan <bhemail14@yahoo.com>
2018-01-14 22:24:16 -0500

    Linked Buttons But Submit
    Add and subtract buttons work.

--- app/src/main/java/com/team20/matchscouting/ScoutScreen.java
+++ app/src/main/java/com/team20/matchscouting/ScoutScreen.java
@@ -2,6 +2,8 @@ package com.team20.matchscouting;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by Ben on 1/7/2018.
@@ -13,5 +15,71 @@ public class ScoutScreen extends Activity{
 
         super.onCreate(saveInstanceState);
         setContentView(R.layout.scout_screen);
+    }
+
+    public void Subtract(View view){
+        TextView tv;
+        switch(view.getId()){
+            case R.id.AutoSwitchMinusButton:
+                tv = (TextView) findViewById(R.id.AutoSwitchTextLabel);
+                if(Integer.valueOf(tv.getText().toString())>0)
+                    tv.setText(Integer.valueOf(tv.getText().toString())-1+"");
+                break;
+            case R.id.AutoScaleMinusButton:
+                tv = (TextView) findViewById(R.id.AutoScaleTextLabel);
+                if(Integer.valueOf(tv.getText().toString())>0)
+                    tv.setText(Integer.valueOf(tv.getText().toString())-1+"");
+                break;
+            case R.id.ExchangeZoneMinusButton:
+                tv = (TextView) findViewById(R.id.ExchangeZoneTextLabel);
+                if(Integer.valueOf(tv.getText().toString())>0)
+                    tv.setText(Integer.valueOf(tv.getText().toString())-1+"");
+                break;
+            case R.id.OSwitchMinusButton:
+                tv = (TextView) findViewById(R.id.OSwitchTextLabel);
+                if(Integer.valueOf(tv.getText().toString())>0)
+                    tv.setText(Integer.valueOf(tv.getText().toString())-1+"");
+                break;
+            case R.id.OpptSwitchMinusButton:
+                tv = (TextView) findViewById(R.id.OpptSwitchTextLabel);
+                if(Integer.valueOf(tv.getText().toString())>0)
+                    tv.setText(Integer.valueOf(tv.getText().toString())-1+"");
+                break;
+            case R.id.ScaleMinusButton:
+                tv = (TextView) findViewById(R.id.ScaleTextLabel);
+                if(Integer.valueOf(tv.getText().toString())>0)
+                    tv.setText(Integer.valueOf(tv.getText().toString())-1+"");
+                break;
+        }
+    }
+
+    public void Add(View view){
+        TextView tv;
+        switch (view.getId()){
+            case R.id.AutoSwitchPlusButton:
+                tv = (TextView) findViewById(R.id.AutoSwitchTextLabel);
+                tv.setText(Integer.valueOf(tv.getText().toString())+1+"");
+                break;
+            case R.id.AutoScalePlusButton:
+                tv = (TextView) findViewById(R.id.AutoScaleTextLabel);
+                tv.setText(Integer.valueOf(tv.getText().toString())+1+"");
+                break;
+            case R.id.ExchangeZonePlusButton:
+                tv = (TextView) findViewById(R.id.ExchangeZoneTextLabel);
+                tv.setText(Integer.valueOf(tv.getText().toString())+1+"");
+                break;
+            case R.id.OSwitchPlusButton:
+                tv = (TextView) findViewById(R.id.OSwitchTextLabel);
+                tv.setText(Integer.valueOf(tv.getText().toString())+1+"");
+                break;
+            case R.id.OpptSwitchPlusButton:
+                tv = (TextView) findViewById(R.id.OpptSwitchTextLabel);
+                tv.setText(Integer.valueOf(tv.getText().toString())+1+"");
+                break;
+            case R.id.ScalePlusButton:
+                tv = (TextView) findViewById(R.id.ScaleTextLabel);
+                tv.setText(Integer.valueOf(tv.getText().toString())+1+"");
+                break;
+        }
     }
 }
