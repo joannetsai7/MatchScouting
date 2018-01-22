f1df4bb3dfd22cc7297287d2c636d67a73701d62
Ben Hogan <bhemail14@yahoo.com>
2018-01-22 12:59:49 -0500

    Main Page
    Starting to rework the main title page elements

--- app/src/main/java/com/team20/matchscouting/MainActivity.java
+++ app/src/main/java/com/team20/matchscouting/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 /**
  * Created by JT on 1/7/18.
@@ -16,7 +17,26 @@ public class MainActivity extends AppCompatActivity{
     }
 
     public void nextButtonOnClick(View view){
+        //Get First Name
+        EditText FirstNameEditText = (EditText) findViewById(R.id.editFirstName);
+        String firstName = FirstNameEditText.getText().toString();
+
+        //Get Last Name
+        EditText LastNameEditText = (EditText) findViewById(R.id.editLastName);
+        String lastName = LastNameEditText.getText().toString();
+
+        //Get Match Number
+        //EditText MatchNumberEditText = (EditText) findViewById(R.id.editMatch);
+        //int matchNumber = Integer.valueOf(MatchNumberEditText.getText().toString());
+
+        //Get Team Number
+        //EditText TeamNumberEditText = (EditText) findViewById(R.id.editTeam);
+        //int teamNumber = Integer.valueOf((MatchNumberEditText.getText().toString()));
+
+        //
+
         Intent intent = new Intent(MainActivity.this,ScoutScreen.class);
+
         startActivity(intent);
     }
 }
